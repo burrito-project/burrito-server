57cbb3267d57c0926b8b474ab7d68d97ae56ef9c
Paolo Flores <pflores.fisi22@gmail.com>
2024-08-17 03:09:19 -0500

    add env utility

--- src/env.rs
+++ src/env.rs
@@ -2,6 +2,6 @@ use lazy_static::lazy_static;
 use std::env;
 
 lazy_static! {
-    pub static ref IS_MOCKED: bool = env::var("IS_MOCKED").unwrap() == "true";
+    pub static ref IS_MOCKED: bool = env::var("IS_MOCKED").unwrap_or("false".into()) == "true";
     pub static ref AUTH_DRIVER_PASSPHRASE: String = env::var("AUTH_DRIVER_PASSPHRASE").unwrap();
 }
