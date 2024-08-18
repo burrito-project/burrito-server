25f3a24787f59807f9ec51356e7385a97811a68d
Paolo Flores <pflores.fisi22@gmail.com>
2024-08-18 05:25:07 -0500

    create initial migration

--- src/db/connect.rs
+++ src/db/connect.rs
@@ -2,8 +2,15 @@ use sqlx::{Error as SqlxError, Pool, Postgres};
 use std::thread::available_parallelism;
 
 pub async fn create_pool() -> Result<Pool<Postgres>, SqlxError> {
-    sqlx::postgres::PgPoolOptions::new()
+    let pool = sqlx::postgres::PgPoolOptions::new()
         .max_connections(available_parallelism().unwrap().get() as u32)
         .connect(crate::env::DATABASE_URL.as_str())
+        .await?;
+
+    sqlx::migrate!("./migrations")
+        .run(&pool)
         .await
+        .expect("Failed to run migrations");
+
+    Ok(pool)
 }
