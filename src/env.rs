729da8151e1ed3cf40a566a0cd629a4eb4e71514
Paolo Flores <pflores.fisi22@gmail.com>
2024-08-17 21:27:30 -0500

    document env variables

--- src/env.rs
+++ src/env.rs
@@ -2,6 +2,20 @@ use lazy_static::lazy_static;
 use std::env;
 
 lazy_static! {
+    // The passphrase to authenticate the bus driver POST requests
+    pub static ref AUTH_DRIVER_PASSPHRASE: String =
+        env::var("AUTH_DRIVER_PASSPHRASE").expect("AUTH_DRIVER_PASSPHRASE must be set");
+
+    // The postgresql database connection string
+    pub static ref DATABASE_URL: String =
+        env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // The max bus state records that we should keep in memory
+    pub static ref MAX_MEMORY_MESSAGES: usize = env::var("MAX_MEMORY_MESSAGES")
+        .unwrap_or("1000".into())
+        .parse::<usize>()
+        .expect("MAX_MEMORY_MESSAGES must be a number");
+
+    /// Whether we should use a mocked bus route to showcase the system. See mock.rs
     pub static ref IS_MOCKED: bool = env::var("IS_MOCKED").unwrap_or("false".into()) == "true";
-    pub static ref AUTH_DRIVER_PASSPHRASE: String = env::var("AUTH_DRIVER_PASSPHRASE").unwrap();
 }
