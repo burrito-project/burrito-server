706d740cc5d2b11553582465f7af221d4de273fc
Paolo Flores <pflores.fisi22@gmail.com>
2024-08-19 16:26:59 -0500

    remove platform_t and add banner_url

--- migrations/0001_app_versions.sql
+++ migrations/0001_app_versions.sql
@@ -1,18 +1,13 @@
-DO $$ BEGIN
-    CREATE TYPE platform_t AS ENUM ('android', 'ios', 'web');
-EXCEPTION
-    WHEN duplicate_object THEN null;
-END $$;
-
 CREATE TABLE IF NOT EXISTS app_versions (
     id serial PRIMARY KEY,
     semver varchar(16) NOT NULL,
     is_mandatory boolean NOT NULL,
+    banner_url text,
     release_notes text,
     release_date timestamp NOT NULL DEFAULT now(),
     updated_at timestamp NOT NULL DEFAULT now(),
     CHECK (semver ~ '^\d+\.\d+\.\d+$'),
-    CHECK (release_date > now())
+    UNIQUE (semver)
 );
 
 CREATE OR REPLACE TRIGGER update_modified_time BEFORE UPDATE ON app_versions FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
