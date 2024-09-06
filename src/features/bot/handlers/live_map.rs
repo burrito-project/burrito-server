43746e16cf4d9ee225ff8b3b32c36c291e0c5bc8
Paolo Flores <pflores.fisi22@gmail.com>
2024-09-06 14:26:47 -0500

    add new whatsapp bot feature

---
+++ src/features/bot/handlers/live_map.rs
@@ -0,0 +1,60 @@
+use image::DynamicImage;
+use include_bytes_plus::include_bytes;
+use lazy_static::lazy_static;
+
+use crate::{bus_stops::LatLng, entities::AppState};
+
+lazy_static! {
+    pub static ref MAP_BASE_IMAGE_BYTES: &'static [u8] =
+        include_bytes!("src/features/bot/assets/map_base.jpg").as_slice();
+    static ref MAP_BASE_IMAGE: image::DynamicImage =
+        image::load_from_memory(*MAP_BASE_IMAGE_BYTES).unwrap();
+    static ref MAP_MARKER_IMAGE: image::DynamicImage = {
+        let bytes = include_bytes!("src/features/bot/assets/burrito_marker.png");
+        image::load_from_memory(bytes.as_slice()).unwrap()
+    };
+}
+
+// This bottom, top, left, right are relative to the original world map
+const MAP_BOTTOM_RIGHT_COORDS: LatLng = LatLng::new(-12.061349549708325, -77.0779032447336);
+const MAP_BOTTOM_LEFT_COORDS: LatLng = LatLng::new(-12.063045246181971, -77.08851784320025);
+const MAP_TOP_RIGHT_COORDS: LatLng = LatLng::new(-12.049784581812744, -77.07985106789113);
+const MAP_TOP_LEFT_COORDS: LatLng = LatLng::new(-12.051563821207822, -77.09050397949446);
+
+/// Returns a live map image with the burrito's location
+///
+/// Returns none if the burrito is not locatable
+pub async fn live_map_handler(state: &AppState) -> Option<DynamicImage> {
+    let burrito_status = crate::api::status::get_burrito_status_impl(1, state).await;
+    let pos = burrito_status.positions[0].clone();
+
+    if !pos.sts.is_locatable() {
+        return None;
+    }
+
+    let mut base_image: image::DynamicImage = MAP_BASE_IMAGE.clone();
+    let marker: image::DynamicImage = MAP_MARKER_IMAGE.clone();
+
+    let a = MAP_BOTTOM_LEFT_COORDS.lng - MAP_TOP_LEFT_COORDS.lng;
+    let b = MAP_BOTTOM_RIGHT_COORDS.lat - MAP_BOTTOM_LEFT_COORDS.lat;
+    let c = MAP_BOTTOM_RIGHT_COORDS.lng - MAP_TOP_RIGHT_COORDS.lng;
+    let d = MAP_TOP_RIGHT_COORDS.lat - MAP_TOP_LEFT_COORDS.lat;
+
+    let x = MAP_TOP_RIGHT_COORDS.lng - MAP_BOTTOM_LEFT_COORDS.lng;
+    let y = MAP_TOP_LEFT_COORDS.lat - MAP_BOTTOM_RIGHT_COORDS.lat;
+
+    let marker_pos_x =
+        base_image.width() as f64 * ((pos.lg - MAP_TOP_LEFT_COORDS.lng) / (a + x + c));
+    let marker_pos_y =
+        base_image.height() as f64 * ((pos.lt - MAP_BOTTOM_LEFT_COORDS.lat) / (b + y + d));
+
+    println!("🐢 marker_pos: {}x{}", marker_pos_x, marker_pos_y);
+    image::imageops::overlay(
+        &mut base_image,
+        &marker,
+        marker_pos_x as i64,
+        marker_pos_y as i64,
+    );
+
+    Some(base_image)
+}
---
+++ src/features/bot/handlers/new_message.rs
@@ -0,0 +1,158 @@
+use rocket::{http::Status, Response, State};
+
+use crate::{
+    core::responses::RawResponse,
+    entities::{AppState, BusServiceState},
+    features::bot::entities::WhatsappMessage,
+};
+
+macro_rules! on_route_just_started_message_template {
+    () => {
+        "🚍 El burrito acaba de iniciar su ruta, vuelve en un momento"
+    };
+}
+
+macro_rules! on_route_stationary_message_template {
+    () => {
+        r#"🚍 En el "{}", recogiendo pasajeros"#
+    };
+}
+
+macro_rules! on_route_moving_message_template {
+    () => {
+        r#"🚍 Yendo hacia "{}"!
+(a {} metros)"#
+    };
+}
+
+macro_rules! out_of_service_message_template {
+    () => {
+        r#"🚍🪧 El transporte interno está fuera de servicio
+Actualizado hace {}"#
+    };
+}
+
+macro_rules! accident_message_template {
+    () => {
+        r#"🚍🪧 El transporte interno está temporalmente fuera de servicio
+Actualizado hace {}"#
+    };
+}
+
+macro_rules! resting_message_template {
+    () => {
+        r#"🚍💤 Los choferes se encuentran en descanso
+Actualizado hace {}"#
+    };
+}
+
+macro_rules! off_message_template {
+    () => {
+        r#"🚍 El burrito está apagado"#
+    };
+}
+
+pub async fn whatsapp_new_text_message_handler(
+    state: &State<AppState>,
+    message: WhatsappMessage,
+) -> RawResponse<'static> {
+    println!("{:#?}", &message);
+
+    let number_id = &message.entry[0].changes[0].value.metadata.phone_number_id;
+    let message = &message.entry[0].changes[0].value.messages[0];
+
+    let burrito_status = crate::api::status::get_burrito_status_impl(1, state).await;
+    let last_stop = &burrito_status.last_stop;
+    let pos = &burrito_status.positions[0];
+    let updated_at = pos.formatted_time_ago();
+
+    let formatted_message = match pos.sts {
+        BusServiceState::OnRoute => {
+            if last_stop.is_none() {
+                on_route_just_started_message_template!().to_string()
+            } else {
+                let last_stop = last_stop.as_ref().unwrap();
+                if last_stop.has_reached {
+                    format!(on_route_stationary_message_template!(), last_stop.name,)
+                } else {
+                    format!(
+                        on_route_moving_message_template!(),
+                        last_stop.name, last_stop.distance as i64,
+                    )
+                }
+            }
+        }
+        BusServiceState::OutOfService => {
+            format!(out_of_service_message_template!(), updated_at)
+        }
+        BusServiceState::Resting => {
+            format!(resting_message_template!(), updated_at)
+        }
+        BusServiceState::Accident => {
+            format!(accident_message_template!(), updated_at)
+        }
+        BusServiceState::Off => off_message_template!().into(),
+    };
+
+    let body = if pos.sts == BusServiceState::OnRoute {
+        serde_json::json!({
+          "messaging_product": "whatsapp",
+          "recipient_type": "individual",
+          "to": message.from,
+          "type": "interactive",
+          "interactive": {
+            "type": "button",
+            "body": {
+              "text": formatted_message,
+            },
+            "action": {
+              "buttons": [
+                {
+                  "type": "reply",
+                  "reply": {
+                    "id": message.id,
+                    "title": "Ver en mapa"
+                  }
+                },
+              ]
+            }
+          }
+        })
+    } else {
+        serde_json::json!({
+          "messaging_product": "whatsapp",
+          "recipient_type": "individual",
+          "to": message.from,
+          "type": "text",
+          "text": {
+            "body": formatted_message,
+          }
+        })
+    };
+
+    let client = reqwest::Client::new();
+    let reply_result = client
+        .post(format!(
+            "https://graph.facebook.com/v20.0/{}/messages",
+            number_id
+        ))
+        .header("content-type", "application/json")
+        .header(
+            "authorization",
+            format!("Bearer {}", crate::env::AUTH_WHATSAPP_ACCESS_TOKEN.as_str()),
+        )
+        .body(body.to_string())
+        .send()
+        .await;
+
+    match reply_result {
+        Ok(w) => {
+            println!("{:#?}", w);
+        }
+        Err(e) => {
+            eprintln!("{:#?}", e);
+        }
+    }
+
+    Response::build().status(Status::Ok).finalize().into()
+}
