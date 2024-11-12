d9c2299343fdd84d6199989a2b30c04527437038
Paolo Flores <pflores.fisi22@gmail.com>
2024-11-11 20:20:26 -0500

    fix: hash invalid fingerprints

--- src/features/identities/handlers/post_session.rs
+++ src/features/identities/handlers/post_session.rs
@@ -1,4 +1,5 @@
 use rocket::State;
+use sha2::{Digest, Sha256};
 use sqlx::types::ipnetwork::IpNetwork;
 
 use crate::core::guards::ForwardedIp;
@@ -10,7 +11,16 @@ pub async fn post_session_handler(
     payload: schemas::UserIdentityPayload,
     state: &State<AppState>,
 ) -> schemas::UserIdentity {
+    let mut payload = payload;
     let user_ip = IpNetwork::new(remote_addr.into(), 32).unwrap();
+
+    // In old versions, we just accepted the fingerprint as is, giving the hashing
+    // responsibility to the client. Now we hash it server-side even if it's already hashed.
+    if payload.fingerprint.len() != 64 || hex::decode(&payload.fingerprint).is_err() {
+        let mut hasher = Sha256::new();
+        hasher.update(payload.fingerprint.as_bytes());
+        payload.fingerprint = hex::encode(&hasher.finalize()[..]);
+    }
 
     sqlx::query_as!(
         schemas::UserIdentity,
